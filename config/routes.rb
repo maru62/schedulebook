5eef60aa94833ba3ccaa513c864f15010b2609d9
*Yuuki <nvos21054@nvrcd.ac.jp>
2022-05-09 13:18:29 +0900

    ルートちょっと書いただけ

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :purchases
   resources :expenses
   resources :messages, constraints: { id: /[0-9]{1,3}/ }
-  resources :days
+  resources :days, constraints: {id: /[0-9]{10}/ }
+  # 今回は日付でURLをつけたいのでIDがそのまま降りてくるURLは使えない
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -18,24 +19,15 @@ Rails.application.routes.draw do
   get 'hellowork/form_with_test'  # これ何だっけ
 
   ## messagesコントローラーのアクションに対してのルート
-  get 'messages/id'
+  get 'messages/id'、、
   get 'messages/search'
   get 'messages/list'
   get 'messages/todesking'
   # 結局Railbookと同じところで躓いて詰まった、なんかルーティングの説明とかをもっと読まないといけない気がする
-  get 'messages/todesking_form'
-  post 'messages/todesking_query'
+  ## 検索して絞り込む機能については全くできてないので保留する
+  ## URLをきちんと整形できてからURLだけ指定したほうがよい気がする
+  # get 'messages/todesking_form'
+  # post 'messages/todesking_query'
   
-  
-=begin 以下はポア  
-  ## とりあえず固定された要求でIDを指定して表示させる
-  get 'messages/find_by_ids'
-
-  ## とりあえず固定された要求で文字列を検索して表示させる
-  get 'messages/find_by_string'
-
-  ## searchコントローラーはポア
-  ### phowa
-=end  
   
 end
